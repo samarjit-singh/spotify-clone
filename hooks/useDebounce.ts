8007d778bdd2d47059b75add8c3a6bc5075e81ff
samarjit-singh <samarjitbravo@gmail.com>
2023-06-18 00:50:20 +0530

    Added search functionality

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+// This will only get the results only after 500 milliseconds of the user not typing anything
+function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
